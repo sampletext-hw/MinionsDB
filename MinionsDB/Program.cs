52cf1c60b9a394ec216866ae6e2aa957cb4eaa79
sampletext32 <sampletext32@bk.ru>
2020-10-01 17:05:30 +0300

    Add project, Implement Task 2 and 3

---
+++ MinionsDB/Program.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+
+namespace MinionsDB
+{
+    class Program
+    {
+        static string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=Minions;Trusted_Connection=True";
+
+        static void Main(string[] args)
+        {
+            Select();
+            //BadInsert();
+            //GoodInsert("Ivan", 54, 1);
+            //GoodSelect("Ivan");
+        }
+
+        /// <summary>
+        /// Самый простой запрос, выборка всех без фильтров
+        /// </summary>
+        static void Select()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            using (connection)
+            {
+                string selectionCommandString = "SELECT * FROM Minions";
+                SqlCommand command = new SqlCommand(selectionCommandString, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write($"{reader[i]} ");
+                        }
+
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Вставка нескольких записей. Так делать плохо, потенциальная SQL инъекция!
+        /// </summary>
+        static void BadInsert()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            using (connection)
+            {
+                string creationCommandString = "INSERT INTO Minions VALUES " +
+                                               "('Kent', 22, 1), " +
+                                               "('Paul', 34, 2)";
+                SqlCommand createCommand = new SqlCommand(creationCommandString, connection);
+                Console.WriteLine(
+                    createCommand.ExecuteNonQuery());
+            }
+        }
+
+        /// <summary>
+        /// Вставка с параметрами, защищаемся от SQL инъекции!
+        /// </summary>
+        static void GoodInsert(string name, int age, int townId)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            using (connection)
+            {
+                SqlCommand cmd = new SqlCommand(
+                    "INSERT INTO Minions " +
+                    "(Name, Age, TownId) VALUES " +
+                    "(@name, @age, @townId)", connection);
+
+                cmd.Parameters.AddWithValue("@name", name);
+                cmd.Parameters.AddWithValue("@age", age);
+                cmd.Parameters.AddWithValue("@townId", townId);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// Выборка с параметрами, защищаемся от SQL инъекции!
+        /// </summary>
+        static void GoodSelect(string name)
+        {
+            string selectionCommandString = $"SELECT * FROM Minions WHERE Name = @name";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(selectionCommandString, connection);
+            SqlParameter parameter = new SqlParameter("@name", SqlDbType.NVarChar, 50) {Value = name};
+            command.Parameters.Add(parameter);
+            //Или параметр можно связать так: 
+            //command.Parameters.AddWithValue("@name", name);
+
+            connection.Open();
+            using (connection)
+            {
+                SqlDataReader reader = command.ExecuteReader();
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write($"{reader[i]} ");
+                        }
+
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MyDbTasks/Program.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+
+namespace MyDbTasks
+{
+    class Program
+    {
+        static string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=Minions;Trusted_Connection=True";
+
+        static void Task1()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            using (connection)
+            {
+                string selectionCommandString =
+                    "SELECT * FROM Villains " +
+                    "WHERE Id IN " +
+                    "(SELECT VillainId " +
+                    "FROM MinionsVillains " +
+                    "GROUP BY VillainId " +
+                    "HAVING COUNT(MinionId) >= 3)";
+                SqlCommand command = new SqlCommand(selectionCommandString, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write($"{reader[i]} ");
+                        }
+
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+
+        private static bool IsVillainExist(int id)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            using (connection)
+            {
+                string selectionCommandString = "SELECT COUNT(*) FROM Villains " +
+                                                "WHERE Id=@id";
+                SqlCommand command = new SqlCommand(selectionCommandString, connection);
+                command.Parameters.AddWithValue("@id", id);
+                var result = (int)command.ExecuteScalar();
+                return result > 0;
+            }
+        }
+
+        private static string GetVillainName(int id)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            using (connection)
+            {
+                string selectionCommandString = "SELECT Name FROM Villains " +
+                                                "WHERE Id=@id";
+                SqlCommand command = new SqlCommand(selectionCommandString, connection);
+                command.Parameters.AddWithValue("@id", id);
+                var result = (string)command.ExecuteScalar();
+                return result;
+            }
+        }
+
+        private static bool IsVillainHasMinions(int id)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            using (connection)
+            {
+                string selectionCommandString = "SELECT COUNT(*) FROM MinionsVillains " +
+                                                "WHERE VillainId=@id";
+                SqlCommand command = new SqlCommand(selectionCommandString, connection);
+                command.Parameters.AddWithValue("@id", id);
+                var result = (int)command.ExecuteScalar();
+                return result > 0;
+            }
+        }
+
+        static void Task2()
+        {
+            Console.Write("Enter VillainId: ");
+            int id = Int32.Parse(Console.ReadLine());
+
+            if (!IsVillainExist(id))
+            {
+                Console.WriteLine($"В базе данных не существует злодея с идентификатором {id}");
+                return;
+            }
+
+            string villainName = GetVillainName(id);
+            Console.WriteLine($"Villain: {villainName}");
+
+            if (!IsVillainHasMinions(id))
+            {
+                Console.WriteLine($"(no minions)");
+                return;
+            }
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            using (connection)
+            {
+                string selectionCommandString =
+                    "SELECT Minions.Name as MinionName, Minions.Age as MinionAge " +
+                    "FROM MinionsVillains " +
+                    "JOIN Villains ON Villains.Id=VillainId " +
+                    "JOIN Minions ON Minions.Id=MinionId " +
+                    "WHERE VillainId=@id " +
+                    "ORDER BY MinionName ASC";
+                SqlCommand command = new SqlCommand(selectionCommandString, connection);
+                command.Parameters.AddWithValue("@id", id);
+                SqlDataReader reader = command.ExecuteReader();
+                int count = 1;
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{count++}. {reader["MinionName"]} {reader["MinionAge"]}");
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Task2();
+        }
+    }
+}
