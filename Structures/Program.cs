e00ae118b0fdcdfced15c3a0159bba359e49aa40
sampletext32 <sampletext32@bk.ru>
2020-10-25 18:39:15 +0300

    Rename project

--- Structures/Program.cs
+++ Structures/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace Structures
+namespace Task1
 {
     public class Program
     {
